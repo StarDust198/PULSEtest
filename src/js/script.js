1f23c85fedd0fcca125a3be5dc71f85e7328f201
StarDust85z <roadtomars2030@gmail.com>
2021-08-25 01:49:59 +0300

    3 new sections & adapt

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
+/*     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/redarrow-left.png"></button>',
@@ -25,5 +25,45 @@ $(document).ready(function(){
                 }
             },
         ]
+    }); */
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
+
+var slider = tns({
+    container: '.carousel__inner',
+    nav: true,
+    controls: false,
+    // items: 1,
+    // slideBy: 1,
+    // autoplay: false,
+    // controlsText: [
+    //     `<img src="icons/redarrow-left.png">`,        
+    //     `<img src="icons/redarrow-right.png">`
+    // ]
+    prevButton: document.querySelector('.prev'),
+    nextButton: document.querySelector('.next'),
+    responsive: {
+        576: {
+            nav: false,
+            controls: true
+        },
+    }
 });
